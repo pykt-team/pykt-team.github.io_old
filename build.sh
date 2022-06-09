414bd37a984039202b1ac7f5ad8013ea6892f26f
meta-tabchen <2808581543@qq.com>
2022-06-09 10:36:47 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
-cd pykt-toolkit && git pull && pip install -e .
+echo Now dictory is $PWD
+git submodule update --remote pykt-toolkit
+#update pykt-toolkit
+cd pykt-toolkit 
+pip install -e .
 cd docs && sh build.sh
+cd ../../
+
+echo Now dictory is $PWD
 rm -r docs 
 cp -r pykt-toolkit/docs/build/html docs
-git add docs
+
+git add docs pykt-toolkit
 git commit -m "update docs"
 git push
